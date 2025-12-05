d4f155c314298c3836bed31c5a49f4c85bcae642
mariamatusiskova <xmatusiskova@stuba.sk>
2025-12-05 03:04:07 +0100

    tensorboard of aspp unet att

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -19,6 +19,7 @@ from .architectures.roinet import RoiNet
 from .architectures.transunet import TransUNet
 from .architectures.transroinet import TransRoiNet
 from .architectures.tiny_swin import TinySwinUNet
+from .architectures.att_unet_aspp import ASPPAttUNet
 from .architectures.swin_transformer_unet_skip_expand_decoder_sys import SwinTransformerSysWrapper
 
 
